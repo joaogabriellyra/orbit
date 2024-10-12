1af47d8c8b9e79627ed8ab4c693158bae4a98b00
joaogabriellyra <jgabriellyra@hotmail.com>
2024-10-12 16:54:20 -0300

    created the goals completed migration

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,3 +8,13 @@ export const goals = pgTable('goals', {
     .notNull()
     .defaultNow(),
 })
+
+export const goalsCompleted = pgTable('goals_completed', {
+  id: text('id').primaryKey(),
+  goalId: text('goal_id')
+    .references(() => goals.id)
+    .notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+})
