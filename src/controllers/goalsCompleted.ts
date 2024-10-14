f8248f5e36e63f8c69eb472b39571beccfa0cca2
joaogabriellyra <jgabriellyra@hotmail.com>
2024-10-14 14:07:51 -0300

    now the application send the goals completed

--- src/controllers/goalsCompleted.ts
+++ src/controllers/goalsCompleted.ts
@@ -1,4 +1,5 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
+import GoalsCompletedService from '../services/goalsCompleted'
 
 export default class GoalsCompletedController {
   private req: FastifyRequest
@@ -8,5 +9,14 @@ export default class GoalsCompletedController {
     this.req = req
     this.reply = reply
     this.service = new GoalsCompletedService()
+  }
+
+  public async getGoalsCompleted() {
+    try {
+      const goalsCompleted = await this.service.getCompletedGoals()
+      this.reply.status(200).send(goalsCompleted)
+    } catch (error) {
+      this.reply.status(500).send({ error })
+    }
   }
 }
--- src/routes/goalsCompleted.ts
+++ src/routes/goalsCompleted.ts
@@ -1,4 +1,5 @@
 import type { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import GoalsCompletedController from '../controllers/goalsCompleted'
 
 export async function goalsCompletedRouter(app: FastifyInstance) {
   app.get('/', async (req: FastifyRequest, reply: FastifyReply) =>
