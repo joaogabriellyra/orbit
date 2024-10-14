3ff4f35f62ee40f8eb05485fbdb4314cda1025b9
joaogabriellyra <jgabriellyra@hotmail.com>
2024-10-14 00:20:48 -0300

    changed the zod schema for desiredWeeklyFrequency

--- src/schemas/goals.ts
+++ src/schemas/goals.ts
@@ -2,5 +2,5 @@ import { z } from 'zod'
 
 export const createGoalBodySchema = z.object({
   title: z.string(),
-  desiredWeeklyFrequency: z.number(),
+  desiredWeeklyFrequency: z.number().int().min(1).max(7),
 })
